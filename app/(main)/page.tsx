e176abcc021ec3df5235178b0bd3cc99ed564c78
Sapper <saniamykhailenko@gmail.com>
2026-04-07 15:22:09 +0300

    add PromoBanners

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero/Hero";
 import css from "./Home.module.css";
+import PromoBanners from "@/components/PromoBanners/PromoBanners";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <PromoBanners />
     </>
   );
 }
---
+++ components/PromoBanners/PromoBanners.tsx
@@ -0,0 +1,55 @@
+import clsx from "clsx";
+import css from "./PromoBanners.module.css";
+import Link from "next/link";
+
+export default function PromoBanners() {
+  return (
+    <section className={css.section}>
+      <div className={clsx("container", css.container)}>
+        <ul className={css.list}>
+          <li className={css.item}>
+            <div className={css.topBox}>
+              <div className={css.countBox}>1</div>
+              <h2 className={css.title}>Huge Sale</h2>
+            </div>
+
+            <div className={css.bottomBox}>
+              <p className={css.discountInterest}>70%</p>
+              <Link href="/medicine?discount=70" className={css.link}>
+                Shop now
+              </Link>
+            </div>
+          </li>
+
+          <li className={css.item}>
+            <div className={css.topBox}>
+              <div className={css.countBox}>2</div>
+              <h2 className={css.title}>Secure delivery</h2>
+            </div>
+
+            <div className={clsx(css.bottomBox, css.centerItem)}>
+              <p className={css.discountInterest}>100%</p>
+              <Link href="/feature" className={css.link}>
+                Read more
+              </Link>
+            </div>
+          </li>
+
+          <li className={css.item}>
+            <div className={css.topBox}>
+              <div className={css.countBox}>3</div>
+              <h2 className={css.title}>Off</h2>
+            </div>
+
+            <div className={css.bottomBox}>
+              <p className={css.discountInterest}>35%</p>
+              <Link href="/medicine?discount=35" className={css.link}>
+                Shop now
+              </Link>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+}
